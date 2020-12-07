940f6c921e9def22da0bac29b6a71fbe36274ed2
MaengMaeng <maybe8189@naver.com>
2020-12-07 17:17:15 +0900

    js, css 파일 임시 생성

---
+++ settings.js
@@ -0,0 +1,12 @@
+const settingsModal = (target, arr) => {
+    const initSettingModal = () => {
+        const modalContainer = createElement('div', 'modal-container');
+        const modalBackground = createElement('div', 'modal-background');
+
+        
+    }
+
+
+
+
+}
