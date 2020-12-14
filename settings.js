73663e58b3cb1e23ef16c3e5a5aac818d1db66fe
MaengMaeng <maybe8189@naver.com>
2020-12-15 08:44:18 +0900

    type 추가, 삭제

--- settings.js
+++ settings.js
@@ -1,46 +1,6 @@
-const initSettingsModal = (arr) => {
+const initSettingsModal = () => {
     const [settingsModal, settingsModalBody, settingsModalButtons] = createModal('설정', ['확인', '취소'], '400px', '300px');
-
-    const bodyContainer = createElement('div', 'settings-body-container');
-
-    for(let i = 0; i < arr.length; i++){
-        const bodyElement = createElement('div', 'settings-body-element');
-        
-        const bodyBox = createElement('div', 'settings-body-box');
-        bodyBox.style.backgroundColor = arr[i][1];
-        
-        const bodyBoxTextContainer = createElement('div', 'settings-body-box-text-container');
-
-        const bodyBoxText = createElement('span', 'settings-body-box-text',arr[i][0]);
-
-        bodyBoxTextContainer.appendChild(bodyBoxText);
-
-        bodyElement.appendChild(bodyBox);
-        bodyElement.appendChild(bodyBoxTextContainer);
-
-        if(i !== 0){
-            const removeButton = createElement('button', 'settings-remove-button', '<img src="./images/black-remove-button.png" />');
-
-            bodyElement.appendChild(removeButton);
-        }
-
-        bodyContainer.appendChild(bodyElement);
-    }
-    
-    const bodyPlusContainer = createElement('div', 'settings-body-plus-container');
-    const bodyPlusImage = createElement('div', 'settings-plus-button', '<img src="./images/black-plus-button.jpg" />');
-    const bodyPlusTextContainer = createElement('div', 'settings-plus-button-text-container');
-    const bodyPlusText = createElement('span', 'settings-plus-button-text', '추가하기');
-
-    bodyPlusTextContainer.appendChild(bodyPlusText);
-    
-    bodyPlusContainer.appendChild(bodyPlusImage);
-    bodyPlusContainer.appendChild(bodyPlusTextContainer);
-
-    bodyContainer.appendChild(bodyPlusContainer);
-
-    settingsModalBody.appendChild(bodyContainer);
-
+    settingsModalBody.id = 'settings-modal-body';
 
     settingsModalButtons[0].addEventListener('click', () => {settingsModal.classList.add('hide');});
     settingsModalButtons[1].addEventListener('click', () => {settingsModal.classList.add('hide');}); 
---
+++ types.js
@@ -0,0 +1,74 @@
+const initTypesModal = () => {
+    const [typesModal, typesModalBody, typesModalButtons, typesModalHeaderText] = createModal('일정 추가', ['확인', '수정', '취소'], '500px', '400px');
+    typesModal.id = 'types-modal';
+
+    typesModalButtons[0].classList.add('hide');
+    typesModalButtons[1].classList.add('hide');
+    
+    const typesNameContainer = createElement('div', 'types-name-container');
+
+    const typesNameSpanContainer = createElement('div', 'types-name-span-container');
+    const typesNameSpan = createElement('span', 'types-name-span', '이름 : ');
+
+    typesNameSpanContainer.appendChild(typesNameSpan);
+
+    const typesNameInputContainer = createElement('div', 'types-name-input-container');
+    const typesNameInput = createElement('input', 'types-name-input');
+    typesNameInput.id = 'typeName';
+
+    typesNameInputContainer.appendChild(typesNameInput);
+
+    typesNameContainer.appendChild(typesNameSpanContainer);
+    typesNameContainer.appendChild(typesNameInputContainer);
+
+    const typesColorContainer = createElement('div', 'types-color-container');
+
+    const typesColorSpanContainer = createElement('div', 'types-color-span-container');
+    const typesColorSpan = createElement('span', 'types-color-span', '색 : ');
+
+    typesColorSpanContainer.appendChild(typesColorSpan);
+
+    const typesColorInputContainer = createElement('div', 'types-color-input-container');
+    const typesColorInput = createElement('input', ['inp', 'types-color-input']);
+    typesColorInput.id = 'colorPicker';
+
+    const typesColorPalette = createElement('div', 'palette');
+    typesColorPalette.id = 'colorPalette';
+
+    typesColorInputContainer.appendChild(typesColorInput);
+    typesColorInputContainer.appendChild(typesColorPalette);
+    
+    typesColorContainer.appendChild(typesColorSpanContainer);
+    typesColorContainer.appendChild(typesColorInputContainer);
+
+    typesModalBody.appendChild(typesNameContainer);
+    typesModalBody.appendChild(typesColorContainer);
+        
+    typesModalButtons[0].addEventListener('click', () => {
+        let name = document.getElementById('typeName').value;
+        let color = document.getElementById('colorPicker').value;
+        types.push([name, color, 1]);
+    
+        setTypes();
+        
+        document.getElementById('type-selection').innerHTML = '';
+        document.getElementById('type-selection').appendChild(createSelection(types, 'type'));
+        
+        drawTypes();
+        drawFooterTypes();
+
+        elements.typesModal.classList.add('hide');
+    });
+
+    typesModalButtons[1].addEventListener('click', () => {
+
+    });
+
+    typesModalButtons[2].addEventListener('click', () => {
+        typesModalButtons[0].classList.add('hide');
+        typesModalButtons[1].classList.add('hide');
+        typesModal.classList.add('hide');
+    }); 
+    
+    return {typesModal, typesModalButtons, typesModalHeaderText};
+}
